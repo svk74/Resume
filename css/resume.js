e867dc9a315960ed63170897a16af3718931ffd9
svk74 <serge.v.kovalev@gmail.com>
2024-11-19 20:05:14 +1000

    start

---
+++ css/resume.js
@@ -0,0 +1,11 @@
+function IsNight() {
+    let Hours = new Date().getUTCHours();
+    
+    let TimeOfDay = (Hours >= 2 && Hours < 7) ? 'День' :
+            (Hours >= 7 && Hours < 12) ? 'Вечер' :
+            (Hours >= 12 && Hours < 21) ? 'У меня ночь, режим "не беспокоить"' :
+            'Утро';
+            
+    var el = document.getElementById('night');
+    el.innerText = TimeOfDay;
+}
