d21bd6d361ba661433963ba454348d84d933f5a0
svk74 <serge.v.kovalev@gmail.com>
2024-11-20 00:02:43 +1000

    недоделан print

--- css/resume.js
+++ css/resume.js
@@ -3,7 +3,7 @@ function IsNight() {
     
     let TimeOfDay = (Hours >= 2 && Hours < 7) ? 'День' :
             (Hours >= 7 && Hours < 12) ? 'Вечер' :
-            (Hours >= 12 && Hours < 21) ? 'У меня ночь, режим "не беспокоить"' :
+            (Hours >= 12 && Hours < 21) ? 'У меня ночь, режим \'не беспокоить\'' :
             'Утро';
             
     var el = document.getElementById('night');
